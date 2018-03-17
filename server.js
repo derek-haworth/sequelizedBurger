beb65b58e86f28cd5421898b90c329cfce2c59fd
Derek Haworth <derek_haworth@bradycorp.com>
2018-03-16 19:55:00 -0500

    restack server.js for sequelize

--- server.js
+++ server.js
@@ -3,17 +3,24 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require("body-parser");
+var methodOverride = require('method-override')
+
+// Sync to the DB before starting the server
+// =============================================================
+var db = require("./models");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ 
@@ -26,6 +33,7 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgerController.js");
 app.use(routes);
 
+var PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
     console.log("I\'m listening... on port " + PORT);
 })
